9717f04d35e3f63284bd9ec460d3d9ef72aa8fc6
khaja.shaik <khaja.shaik@192.168.1.4>
2020-01-26 23:33:16 +0530

    Check-in for Algorithms.

---
+++ Algorithms/src/main/java/com/algorithms/datastructures/hashtable/SecondaryHash.java
@@ -0,0 +1,10 @@
+/**
+ * Simple interface file for double hashing
+ */
+package com.algorithms.datastructures.hashtable;
+
+public interface SecondaryHash {
+
+  // A method for a second hash function
+  public int hashCode2();
+}
